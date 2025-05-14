87f121d7fe0997776c85e8c1e2a1f5e7fff8cb19
aqsari <9d.aqsari.nufikha.putri.05@gmail.com>
2025-05-14 09:23:24 +0700

    update

--- basicsr/version.py
+++ basicsr/version.py
@@ -1,5 +1,5 @@
 # GENERATED VERSION FILE
-# TIME: Sun May  4 14:09:18 2025
-__version__ = '1.2.0+2b4af71'
+# TIME: Mon May  5 15:34:58 2025
+__version__ = '1.2.0+'
 short_version = '1.2.0'
 version_info = (1, 2, 0)
